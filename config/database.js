3bef19698e35757047aff5d0c98db08ca7f0fa0d
Vatsal Patel <113112670+pvatsalk@users.noreply.github.com>
2023-12-13 20:50:37 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@ require('dotenv').config()
 const mongo = process.env.MONGODB_URI;
 
 module.exports = {
-    url: mongo + "mongodb+srv://vatsalpatel1841103:Ram%4013579@vpcluster.hexdv.mongodb.net/sample_mflix"
+    url: mongo + "/sample_mflix"
 };
